466ac9bdc58c100d515a8494b890bb0b907412df
Artem <63448341+Gilah-EnE@users.noreply.github.com>
2023-08-03 23:45:49 +0300

    Implement Stack using Queues + Flood Fill formatting

--- Flood Fill.py
+++ Flood Fill.py
@@ -2,11 +2,18 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         if image[sr][sc] != color:
             self.fill(image, sr, sc, color, image[sr][sc])
-        
+
         return image
-    
+
     def fill(self, image, sr, sc, newcolor, color):
-        if sr < 0 or sc < 0 or sr >= len(image) or sc >= len(image[0]) or color != image[sr][sc] or newcolor == image[sr][sc]:
+        if (
+            sr < 0
+            or sc < 0
+            or sr >= len(image)
+            or sc >= len(image[0])
+            or color != image[sr][sc]
+            or newcolor == image[sr][sc]
+        ):
             return None
 
         image[sr][sc] = newcolor
